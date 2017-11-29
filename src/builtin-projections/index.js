87c791f1c450d5f0bd21fb831a39876ea9784e19
Yucong Jin <jinyucong911@outlook.com>
2017-11-29 06:47:40 -0800

    add sortable

--- src/builtin-projections/index.js
+++ src/builtin-projections/index.js
@@ -2,10 +2,12 @@ import composer from './composer';
 import defaults from './defaults';
 import customColumn from './custom-column';
 import customCaption from './custom-caption';
+import sortable from './sortable';
 
 export default [
   defaults,
   composer,
   customColumn,
   customCaption,
+  sortable,
 ];
