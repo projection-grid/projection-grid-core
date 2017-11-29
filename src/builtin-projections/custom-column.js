6b9696f4d23ad0a6afc5179c91d61a76014c2f16
Wei Wei <lyweiwei@outlook.com>
2017-11-29 10:12:53 +0800

    Refactor custom column

--- src/builtin-projections/custom-column.js
+++ src/builtin-projections/custom-column.js
@@ -1,35 +1,42 @@
 import _ from 'underscore';
 
+/*
+Configuration added by custom-column projection
+
+  column.Component
+  column.attributes
+  column.events
+*/
+
 export default function customColumn(config) {
   const configNew = _.defaults({
     composeTds(options) {
-      return _.chain(config.composeTds(options))
-        .map((td) => {
-          const { attributes, content } = td;
-          const { events } = content;
-          const { column } = options;
-          const { Component } = column;
+      const { column } = options;
+      const customAttributes = _.mapObject(
+        column.attribute || {},
+        attr => (_.isFunction(attr) ? attr(options) : attr)
+      );
+      const customEvents = _.mapObject(
+        column.events || {},
+        handler => (...args) => handler(options, ...args)
+      );
+      const wrapContent = column.Component ?
+        // If the custom Component is provided, wrap the content with it.
+        // The custom events is for the custom Component
+        content => ({
+          Component: column.Component,
+          props: _.defaults({ content }, options),
+          events: customEvents,
+        }) :
+        // Otherwise the events is for the original component
+        content => _.defaults({
+          events: _.defaults({}, customEvents, content.events),
+        }, content);
 
-          return _.defaults({
-            attributes: _.chain(column)
-              .result('attributes')
-              .mapObject(attr => attr(options))
-              .defaults(attributes)
-              .value(),
-            content: Component ? {
-              Component,
-              props: _.defaults({ content }, options),
-              events: _.chain(column)
-                .result('events')
-                .mapObject(handler => (...args) => handler(options, ...args))
-                .defaults(events)
-                .value(),
-            } : content,
-          }, td);
-        })
-        .flatten()
-        .compact()
-        .value();
+      return _.map(config.composeTds(options), td => _.defaults({
+        attributes: _.defaults({}, customAttributes, td.attributes),
+        content: wrapContent(td.content),
+      }, td));
     },
   }, config);
 
