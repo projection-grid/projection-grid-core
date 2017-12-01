ed0d7f8dfe7f1c2a4df19f879684acd64c13e518
Wei Wei <lyweiwei@outlook.com>
2017-12-01 11:50:41 +0800

    Remove old builtin projctions

--- src/builtin-projections-old/composer/compose-caption.js
+++
@@ -1,9 +0,0 @@
-import _ from 'underscore';
-
-export function composeCaption({
-  config: { caption, composeContent },
-}) {
-  return caption ? _.defaults({
-    content: composeContent(_.pick(caption.content, 'props', 'events')),
-  }, _.pick(caption, 'props', 'classes', 'styles', 'events')) : null;
-}
--- src/builtin-projections-old/composer/compose-tfoot.js
+++
@@ -1,3 +0,0 @@
-export function composeTfoot() {
-  return null;
-}
