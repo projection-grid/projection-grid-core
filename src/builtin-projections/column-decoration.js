e1ac012b5f64cb9466cf9fee9f079c16a48c3e2e
Wei Wei <lyweiwei@outlook.com>
2017-12-01 22:22:19 +0800

    Fix bugs in column-decoration projection

--- src/builtin-projections/column-decoration.js
+++ src/builtin-projections/column-decoration.js
@@ -3,16 +3,16 @@ import { Decorator } from '../utils/decorator';
 import { COMMON_PROPS } from '../constants';
 
 const decorations = {
-  composeCols: [],
-  composeHeaderThs: ['content'],
-  composeDataTds: ['content'],
+  composeCols: { key: 'col', props: [] },
+  composeHeaderThs: { key: 'th', props: ['content'] },
+  composeDataTds: { key: 'td', props: ['content'] },
 };
 
 export function columnDecoration(composer) {
   const orig = _.clone(composer);
 
-  return _.mapObject(decorations, (props, name) => (context) => {
-    const deco = Decorator.create(context.column.col, COMMON_PROPS.concat(props));
+  return _.mapObject(decorations, ({ key, props }, name) => (context) => {
+    const deco = Decorator.create(context.column[key], COMMON_PROPS.concat(props));
     const model = deco(context, orig[name](context));
 
     return model;
