5ca5e4cceca294c0a6c6b9086371fa678931a241
Yucong Jin <jinyucong911@outlook.com>
2017-12-04 04:22:41 -0800

    striped rows

--- src/builtin-projections/theme.js
+++ src/builtin-projections/theme.js
@@ -1,20 +1,26 @@
 
-const extendStyles = (memo, styles) => Object.assign({}, memo, {
+const applyStyles = (memo, styles) => Object.assign({}, memo, {
   styles: Object.assign({}, memo.styles, styles),
 });
 
-const applyStylsForFirstChild = (parent, styles, childPath = 'trs/tds') => {
+const applyStylesForChildByCondition = (
+  parent,
+  styles,
+  childPath = 'trs/tds',
+  condition = () => false
+) => {
   const [, path1, path2] = childPath.match(/^(\S+)\/(\S+)$/);
 
   if (Array.isArray(parent)) {
-    return parent.map(parentMemo => applyStylsForFirstChild(parentMemo, styles, childPath));
+    return parent.map(parentMemo =>
+      applyStylesForChildByCondition(parentMemo, styles, childPath, condition));
   }
 
   return Object.assign({}, parent, {
     [path1]: parent[path1].map((childLevel1, index) => {
-      if (index === 0) {
+      if (condition(childLevel1, index)) {
         return Object.assign({}, childLevel1, {
-          [path2]: childLevel1[path2].map(childLevel2 => extendStyles(childLevel2, styles)),
+          [path2]: childLevel1[path2].map(childLevel2 => applyStyles(childLevel2, styles)),
         });
       }
 
@@ -23,18 +29,22 @@ const applyStylsForFirstChild = (parent, styles, childPath = 'trs/tds') => {
   });
 };
 
-const compseWithBootstrap = ({
+const applyStylsForFirstChild = (parent, styles, childPath = 'trs/tds') =>
+  applyStylesForChildByCondition(parent, styles, childPath, (child, index) => index === 0);
+
+const applyStylsForOddChild = (parent, styles, childPath = 'trs/tds') =>
+  applyStylesForChildByCondition(parent, styles, childPath, (child, index) => !(index % 2));
+
+const composeWithBootstrap = ({
   composeTable,
   composeCaption,
   composeHeaderThs,
   composeDataTds,
-  // composeHeaderTrs,
-  // composeDataTrs,
   composeTbodies,
   composeThead,
 }) => ({
   composeTable(table) {
-    return extendStyles(composeTable(table), {
+    return applyStyles(composeTable(table), {
       width: '100%',
       maxWidth: '100%',
       backgroundColor: 'transparent',
@@ -43,7 +53,7 @@ const compseWithBootstrap = ({
     });
   },
   composeCaption(caption) {
-    return extendStyles(composeCaption(caption), {
+    return applyStyles(composeCaption(caption), {
       paddingTop: '8px',
       paddingBottom: '8px',
       color: '#777',
@@ -51,7 +61,7 @@ const compseWithBootstrap = ({
     });
   },
   composeHeaderThs(ths) {
-    return composeHeaderThs(ths).map(th => extendStyles(th, {
+    return composeHeaderThs(ths).map(th => applyStyles(th, {
       verticalAlign: 'bottom',
       borderBottom: '2px solid #ddd',
       textAlign: 'left',
@@ -61,7 +71,7 @@ const compseWithBootstrap = ({
     }));
   },
   composeDataTds(tds) {
-    return composeDataTds(tds).map(td => extendStyles(td, {
+    return composeDataTds(tds).map(td => applyStyles(td, {
       padding: '8px',
       inlineHeight: 1.42857143,
       verticalAlign: 'top',
@@ -74,22 +84,37 @@ const compseWithBootstrap = ({
   composeThead(thead) {
     return applyStylsForFirstChild(composeThead(thead), { borderTop: 0 }, 'trs/ths');
   },
-  // composeHeaderTrs(trs) {
-  //   return applyStylsForFirstChild(composeHeaderTrs(trs), { borderTop: 0 }, 'ths');
-  // },
-  // composeDataTrs(trs) {
-  //   window.console.log(composeDataTrs(trs));
-
-  //   return applyStylsForFirstChild(composeDataTrs(trs), { borderTop: 0 });
-  // },
 });
+
+const composeWithBootstrapStripedRows = ({
+  composeTable,
+  composeCaption,
+  composeHeaderThs,
+  composeDataTds,
+  composeTbodies,
+  composeThead,
+}) => Object.assign({}, composeWithBootstrap({
+  composeTable,
+  composeCaption,
+  composeHeaderThs,
+  composeDataTds,
+  composeTbodies,
+  composeThead,
+}), {
+  composeTbodies(tbody) {
+    return applyStylsForOddChild(composeTbodies(tbody), { backgroundColor: '#f9f9f9' });
+  },
+});
+
 
 export function theme(model, config) {
   switch (config.theme) {
     case 'bootstrap':
     case 'bootstrap3':
     case 'bootstrap-default':
-      return compseWithBootstrap(model);
+      return composeWithBootstrap(model);
+    case 'bootstrap-striped-rows':
+      return composeWithBootstrapStripedRows(model);
     default:
       return {};
   }
