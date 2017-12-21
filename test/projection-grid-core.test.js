c30fc4465c10d340588018d430f625ed2c86fc25
Wei Wei <lyweiwei@outlook.com>
2017-12-21 17:04:26 +0800

    Refactor projection-grid-core class

---
+++ test/projection-grid-core.test.js
@@ -0,0 +1,30 @@
+import { ProjectionGridCore } from '../src/projection-grid-core';
+import { DEFAULT_COMMON, DEFAULT_TABLE } from './io-test/constants';
+
+describe('ProjectionGridCore#compose', () => {
+  test('compose empty grid', () => {
+    const core = new ProjectionGridCore();
+    expect(core.compose({ config: {} })).toEqual({
+      table: {},
+    });
+  });
+
+  test('compose default grid', () => {
+    const core = ProjectionGridCore.createDefault();
+    expect(core.compose({ config: {} })).toEqual({
+      table: {
+        ...DEFAULT_TABLE,
+        thead: {
+          ...DEFAULT_COMMON,
+          tag: 'THEAD',
+          trs: [{
+            ...DEFAULT_COMMON,
+            key: '@header',
+            tag: 'TR',
+            tds: [],
+          }],
+        },
+      },
+    });
+  });
+});
