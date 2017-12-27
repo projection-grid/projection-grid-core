d21242c4ec2652b044db071c6a01fd4a135834cb
Wei Wei <lyweiwei@outlook.com>
2017-12-27 15:17:33 +0800

    Fix test

--- test/io-test/io-test.js
+++ test/io-test/io-test.js
@@ -3,17 +3,11 @@ import { scenarioTest } from './scenario-test';
 export function ioTest({
   name,
   projections,
-<<<<<<< HEAD
-=======
-  postProjections = [],
-  method = 'composeTable',
->>>>>>> team/master
   input = {},
   output,
   matchObject,
   validate,
 }) {
-<<<<<<< HEAD
   scenarioTest({
     name,
     projections,
@@ -23,17 +17,5 @@ export function ioTest({
       strictMatch: !matchObject,
       validate,
     }],
-=======
-  describe('Input/Output Test', () => {
-    test(name, () => {
-      const output = composer([...projections, ...postProjections])[method](input);
-      if (matchObject) {
-        expect(output).toMatchObject(expected);
-      } else {
-        expect(output).toEqual(expected);
-      }
-      validate({ input, output });
-    });
->>>>>>> team/master
   });
 }
